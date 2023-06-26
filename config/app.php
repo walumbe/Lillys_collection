92e2cf0a11e96305c415552521673d3d3fa962e0
walumbe <nathanwalumbe@gmail.com>
2023-06-26 09:33:01 +0300

    debug blacklist

--- config/app.php
+++ config/app.php
@@ -212,4 +212,18 @@ return [
         // 'ExampleClass' => App\Example\ExampleClass::class,
     ])->toArray(),
 
+    'debug_blacklist' => [
+        '_ENV' => [
+            'MPESA_CONSUMER_KEY',
+            'MPESA_CONSUMER_SECRET',
+            'MPESA_SHORTCODE',
+            'MPESA_STK_SHORTCODE',
+            'MPESA_ENV',
+            'MPESA_TEST_MSISDN',
+            'MPESA_TEST_URL',
+            'MPESA_PASSKEY',
+            'MPESA_B2C_PASSWORD'
+        ]
+    ]
+
 ];
