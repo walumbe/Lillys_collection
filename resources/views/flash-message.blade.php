c617ad6b04c7ed1480c4536394bec3dd8490f09a
Sisionkitiyio <Sisionkitiyio@gmail.com>
2023-06-28 09:09:45 +0300

    bug fixes

--- resources/views/flash-message.blade.php
+++ resources/views/flash-message.blade.php
@@ -1,6 +1,13 @@
 
-@if(session('message'))
-    <div class="alert alert-success">
-        {{ session('message') }}
+@if(session()->has ('success'))
+    <div class="alert alert-success alert-block">
+        <button type="button" class="close"data-dismiss="alert">×</button>
+       <strong> {{ session()->get('success') }}</strong>
+    </div>
+
+    @if(session()->has ('error'))
+    <div class="alert alert-danger alert-block">
+        <button type="button" class="close"data-dismiss="alert">×</button>
+       <strong> {{ session()->get('success') }}</strong>
     </div>
 @endif
