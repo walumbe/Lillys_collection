799dc532ff082a77b57ebd88bcc9c4dfad7ce019
Sisionkitiyio <Sisionkitiyio@gmail.com>
2023-06-30 18:17:30 +0300

    bug fixes

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -7,7 +7,7 @@
 
                  <div class="text-center text-white">
                     <h1 class="display-4 fw-bolder">Shop in style</h1>
-                    <p class="lead fw-normal text-white-50 mb-0">With this shop hompeage template</p>
+                    <p class="lead fw-normal text-white-50 mb-0">With lillys collection</p>
                 </div> 
              </div>
         </header> 
